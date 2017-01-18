3e31bfd4e3b6d11f35ea2a3373cdedb7f5be95b5
maratumba <dandik@gmail.com>
2017-01-18 16:52:04 +0300

    Working version

---
+++ compDelayXcorr.py
@@ -0,0 +1,231 @@
+#!/usr/bin/python
+# cross correlate seismogram pairs and print out delays
+# 
+# awk 'NF<6 {split($1,a,".");split($2,b,".");print a[3],b[3],$5,$0}' xcorDelays4.txt > xcorDelays4Formatted.txt
+import argparse
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser(description='Calculates differential arrival times')
+    parser.add_argument('-e','--evinfo',type=str,help='file with event information',required=True)
+    parser.add_argument('-o','--outfile',type=str,help='write output to a file')
+    parser.add_argument('FILES',nargs='+')
+    args=parser.parse_args()
+
+import glob
+import obspy
+import obspy.signal.cross_correlation
+from obspy.core import read
+import scipy.signal
+import numpy as np
+from obspy.taup.taup import getTravelTimes
+from obspy.core.utcdatetime import UTCDateTime
+import os
+import sys
+import pickle
+  
+def haversine(lat1,lon1,lat2,lon2):
+    """
+    haversine(lat1,lon1,lat2,lon2):
+    Return distance in kilometers
+    """
+    
+    
+    import math
+    R = 6372.8
+    # In kilometers
+    dLat = math.radians(lat2 - lat1)
+    dLon = math.radians(lon2 - lon1)
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+    
+    a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.sin(dLon / 2) * math.sin(dLon / 2) * math.cos(lat1) * math.cos(lat2)
+    c = 2 * math.asin(math.sqrt(a))
+    return R * c
+
+def haversineD(lat1,lon1,lat2,lon2):
+    """
+    haversine(lat1,lon1,lat2,lon2):
+    Return distance in kilometers
+    """
+    
+    
+    import math
+    R = 6372.8
+    # In kilometers
+    dLat = math.radians(lat2 - lat1)
+    dLon = math.radians(lon2 - lon1)
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+    
+    a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.sin(dLon / 2) * math.sin(dLon / 2) * math.cos(lat1) * math.cos(lat2)
+    c = 2 * math.asin(math.sqrt(a))
+    return c * 180 / math.pi
+
+def parseEventDb(eventDbFile):
+  #parse event catalog into a dictionary 
+  colnames=['year','julday','time','depth','evla','evlo','evmag','evmag2']
+  dtypes=['i','i','S8','f','f','f','f','f']
+  dtype=zip(colnames,dtypes)
+  evdb=np.genfromtxt(eventDbFile, dtype=dtype,)
+  evdict={}
+  
+  for ev in evdb:
+    dirname="_".join([str(ev['year']),"%03d" % ev['julday'],ev['time'],"M"+"%2.1f"%ev['evmag']])
+    evdict[dirname]=ev
+  
+  
+  return evdict
+
+if __name__=='__main__':
+
+    #sampling rate for cross correlation
+    #crossDelta=0.001
+    crossDelta=0.005
+    
+    #before and after wrt arrival time in seconds
+    before=10.0
+    after=20.0
+    
+    #cross correlation shift length
+    #1/2 * window length
+    #shiftlen=7500
+    shiftlen=int(((before+after)/crossDelta)/4)
+    
+    stcut=obspy.core.stream.Stream()   
+    #delays list
+    delays=[]
+    
+    delaydict={}
+    
+    #parse event catalog
+    evdict=parseEventDb(args.evinfo)
+    dirname=os.getcwd().split('/')[-1]
+    try:
+        ev=evdict[dirname]
+    except:
+        print "ERROR: event "+dirname+" is not in the database, skipping"
+        sys.exit(1)
+    
+    evyear=int(ev['year'])
+    evjulday=int(ev['julday'])
+    evhour=int(ev['time'][0:2])
+    evmin=int(ev['time'][2:4])
+    evsec=int(ev['time'][4:6])
+    evmsec=int(ev['time'][7:8])*1000
+    evtime=UTCDateTime(year=evyear,julday=evjulday,hour=evhour,minute=evmin,second=evsec,microsecond=evmsec)
+
+  
+    for bhz1File in args.FILES:
+      print "reading",bhz1File
+      st1=read(bhz1File)
+      tr1=st1[0]
+      
+      delta=tr1.stats.delta
+      #remove mean:
+      tr1.data=tr1.data-tr1.stats.sac.depmen
+      
+      #remove data before B sac header value
+      #tr1.data=tr1.data[-tr1.stats.sac.b/tr1.stats.delta:]
+      try:
+        #calculate GCARC (for some reason obspy does not read gcarc header)
+        gcarc1=haversineD(tr1.stats.sac.evla, tr1.stats.sac.evlo, tr1.stats.sac.stla, tr1.stats.sac.stlo)
+      except:
+        print "WARNING: Can\'t calculate event distance, quitting",tr1
+        continue
+      
+        
+        print "delta=",gcarc1,"depth=",tr1.stats.sac.evdp
+      #t1 = np.arange(0, tr1.stats.npts / tr1.stats.sampling_rate, tr1.stats.delta)
+      
+      
+      try:
+        tt1=getTravelTimes(delta=gcarc1, depth=tr1.stats.sac.evdp, model='ak135',phase_list=['P'])[0]['time']
+      except:
+        print "WARNING: Can\'t calculate travel time, skipping",tr1
+        continue
+      
+
+      tr1.stats.sac.t1=evtime+tt1
+      tr1.trim(tr1.stats.sac.t1-before, tr1.stats.sac.t1+after)
+      crossNpts=len(tr1.data)/(crossDelta/tr1.stats.delta)
+      #sys.exit()
+      crossData1=scipy.signal.resample(tr1.data,crossNpts)
+      if tr1.stats.station not in delaydict:
+          delaydict[tr1.stats.station]={}
+      
+      for bhz2File in args.FILES:
+        print "reading",bhz2File        
+        st2=read(bhz2File)
+        tr2=st2[0]
+        #tr2.stats.delays=[]
+        #print "st1: ",st1
+        #print "st2: ",st2
+        #if tr1.stats.sac.t1==-12345 or \
+        #tr2.stats.sac.t1==-12345 or \
+        # print bhz1File,".t1: ",tr1.stats.sac.t1,bhz2File,".t1: ",bhz1File,".t2: ",tr1.stats.sac.t2,bhz2File,".t2: ",tr2.stats.sac.t2
+        # continue
+        
+
+        
+            
+        
+        #print "crossData1= ",-tr1.stats.sac.b/crossDelta
+        #print "crossData2= ",-tr2.stats.sac.b/crossDelta
+    
+        #print "tr1.stats.sac.b ", tr1.stats.sac.b, ", tr2.stats.sac.b ", tr2.stats.sac.b
+        #print "crossData1: ",len(crossData1)
+        #print "crossData2: ",len(crossData2)
+        
+        
+        try:
+          #calculate GCARC (for some reason obspy does not read gcarc header)
+          gcarc2=haversineD(tr1.stats.sac.evla, tr1.stats.sac.evlo, tr2.stats.sac.stla, tr2.stats.sac.stlo)
+        except:
+          print "WARNING: Can\'t calculate event distance, skipping",tr2
+          continue
+          
+          print "delta=",gcarc2,"depth=",tr2.stats.sac.evdp
+        try:
+          tt2=getTravelTimes(delta=gcarc2, depth=tr2.stats.sac.evdp, model='ak135',phase_list=['P'])[0]['time']
+        except:
+          print "WARNING: Can\'t calculate event distance, skipping",tr2
+          continue
+        
+        tr2.stats.sac.t1=evtime+tt2
+        tr2.data=tr2.data-tr2.stats.sac.depmen
+        #tr2.data=tr2.data[-tr2.stats.sac.b/tr2.stats.delta:]
+        tr2.trim(tr1.stats.sac.t1-before, tr1.stats.sac.t1+after)
+         
+                #make sure start times are equal
+        if tr1.stats.starttime!=tr2.stats.starttime:
+          #I can fix this
+          print "start times are not synced, skipping"
+          continue 
+           
+        crossData2=scipy.signal.resample(tr2.data,crossNpts)
+        
+        maxInd,maxval=obspy.signal.cross_correlation.xcorr(crossData1,crossData2,shiftlen)
+        
+        t2del=maxInd*crossDelta
+        t1del=tr1.stats.sac.t1 - tr2.stats.sac.t1
+        
+        deldel=t2del-t1del
+        #delays.append([tr1.__str__(),tr2.__str__(),tr1.stats.station,tr2.stats.station,t2del,t1del,deldel,maxval])
+        #stcut.append(tr2)
+        pairstr=tr1.stats.station+'_'+tr2.stats.station
+        if pairstr not in delaydict[tr1.stats.station]:
+            delaydict[tr1.stats.station][tr2.stats.station]=[[],{}]
+        delaydict[tr1.stats.station][tr2.stats.station][1]['t1del']=t1del    
+        delaydict[tr1.stats.station][tr2.stats.station][1]['t2del']=t2del
+        delaydict[tr1.stats.station][tr2.stats.station][1]['deldel']=deldel
+        delaydict[tr1.stats.station][tr2.stats.station][1]['maxval']=maxval
+        stsum=obspy.core.stream.Stream()
+        stsum.append(tr1)
+        stsum.append(tr2)
+        delaydict[tr1.stats.station][tr2.stats.station].append(stsum)
+        
+        print bhz1File, bhz2File, t2del, t1del, deldel, maxval
+        #print tr1.stats.station, tr2.stats.station, t2del, t1del, deldel
+
+    
+    #print delays
